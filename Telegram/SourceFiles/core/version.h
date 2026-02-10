43425552961594ae0c5effef5c7a279403818cc2
DyingLay <slashto488@protonmail.com>
2026-02-10 13:47:34 +0300

    fix: project name during rebase

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -19,10 +19,10 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 // used in Updater.cpp and Setup.iss for Windows
 constexpr auto AppId = "{53F49750-6209-4FBF-9CA8-7A333C87D1ED}"_cs;
-constexpr auto AppNameOld = "Telegram Win (Unofficial)"_cs;
-constexpr auto AppName = "Telegram Desktop"_cs;
-constexpr auto AppFile = "Telegram"_cs;
-constexpr auto AppVersion = 6007005;
-constexpr auto AppVersionStr = "6.7.5";
+constexpr auto AppNameOld = "AhiGram"_cs;
+constexpr auto AppName = "AhiGram Desktop"_cs;
+constexpr auto AppFile = "AhiGram"_cs;
+constexpr auto AppVersion = 6005001;
+constexpr auto AppVersionStr = "6.5.1";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
