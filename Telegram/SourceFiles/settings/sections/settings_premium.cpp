a38345bd5e47dd1f836b0269e85e7db4ad614559
DyingLay <slashto488@protonmail.com>
2026-02-13 13:48:18 +0300

    feat: improve privacy and refactor resources

--- Telegram/SourceFiles/settings/sections/settings_premium.cpp
+++ Telegram/SourceFiles/settings/sections/settings_premium.cpp
@@ -467,7 +467,8 @@ void SendAppLog(
 		not_null<::Main::Session*> session,
 		const QString &type,
 		const MTPJSONValue &data) {
-	const auto now = double(base::unixtime::now())
+	return;
+	/* const auto now = double(base::unixtime::now())
 		+ (QTime::currentTime().msec() / 1000.);
 	session->api().request(MTPhelp_SaveAppLog(
 		MTP_vector<MTPInputAppEvent>(1, MTP_inputAppEvent(
@@ -476,7 +477,7 @@ void SendAppLog(
 			MTP_long(0),
 			data
 		))
-	)).send();
+	)).send();*/
 }
 
 [[nodiscard]] QString ResolveRef(const QString &ref) {
